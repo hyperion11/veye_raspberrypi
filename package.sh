a03962ed6fc8f725afd6900aaf4448bcddeae50a
Stephen Oliver <steve@infincia.com>
2020-06-20 13:41:13 -0400

    Add package script for OpenHD

---
+++ package.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+PLATFORM=$1
+DISTRO=$2
+
+
+if [[ "${PLATFORM}" == "pi" ]]; then
+    OS="raspbian"
+    ARCH="arm"
+    PACKAGE_ARCH="armhf"
+fi
+
+
+PACKAGE_NAME=veye-raspberrypi
+
+TMPDIR=/tmp/${PACKAGE_NAME}-installdir
+
+rm -rf ${TMPDIR}/*
+
+mkdir -p ${TMPDIR}/usr/local/bin || exit 1
+mkdir -p ${TMPDIR}/usr/local/share/veye-raspberrypi || exit 1
+
+cp -a veye_raspcam/bin/veye* ${TMPDIR}/usr/local/bin/
+cp -a i2c_cmd/bin/* ${TMPDIR}/usr/local/share/veye-raspberrypi/
+
+VERSION=$(git describe)
+
+rm ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
+
+fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TMPDIR} \
+  -p ${PACKAGE_NAME}_VERSION_ARCH.deb || exit 1
+
+#
+# Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
+# don't create a tag. You can build packages and test them locally without tagging.
+#
+git describe --exact-match HEAD > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    echo "Pushing package to OpenHD repository"
+    cloudsmith push deb openhd/openhd/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
+else
+    echo "Not a tagged release, skipping push to OpenHD repository"
+fi
