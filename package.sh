6243fb4df2a467c5bb9d91be91de8eb3c133c2d5
MacDaddyFPV <63026018+MacDaddyFPV@users.noreply.github.com>
2022-02-27 22:38:10 +1100

    Add 2.1 cloudsmith to updates
    Adding the 2.1 repositories so they get updated in line with the latest VEYE.

--- package.sh
+++ package.sh
@@ -56,3 +56,10 @@ else
     echo "Pushing package to OpenHD testing repository"
     cloudsmith push deb openhd/openhd-2-0-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
 fi
+if [[ $? -eq 0 ]]; then
+    echo "Pushing package to OpenHD 2.1 repository"
+    cloudsmith push deb openhd/openhd-2-1/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
+else
+    echo "Pushing package to OpenHD 2.1 testing repository"
+    cloudsmith push deb openhd/openhd-2-1-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb || exit 1
+fi
