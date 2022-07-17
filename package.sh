d22a51b4a0b96290011453e2ba27a316a0f52119
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-17 14:29:25 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -26,10 +26,10 @@ cd /data/
 rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
 
 sudo fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION} -C ${TMPDIR} \
-  -p ${PACKAGE_NAME}_VERSION_ARCH.deb || exit 1
+  -p /home/runner/work/veye_raspberrypi/veye_raspberrypi/${PACKAGE_NAME}_VERSION_ARCH.deb || exit 1
 
 #
 # Only push to cloudsmith for tags. If you don't want something to be pushed to the repo, 
 # don't create a tag. You can build packages and test them locally without tagging.
 #
-git describe --exact-match HEAD > /dev/null 2>&1
+cd /home/runner/work/veye_raspberrypi/veye_raspberrypi
