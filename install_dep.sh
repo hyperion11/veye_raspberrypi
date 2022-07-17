9d8ef20973872691a94fba76f01ecefa71b2e34e
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-17 14:18:32 +0200

    add dependencies

--- install_dep.sh
+++ install_dep.sh
@@ -4,23 +4,8 @@
 # TODO do we need libgstreamer1.0-dev and libgstreamer-plugins-base1.0-dev ?
    
 
-apt -y install build-essential autotools-dev automake libtool autoconf \
-            libpcap-dev libsodium-dev \
-            libboost-all-dev libasio-dev \
-            libgstreamer-plugins-base1.0-dev \
-            libusb-1.0-0-dev \
-            libv4l-dev \
-            apt-transport-https \
-            curl \
-            git \
-            libc6-dev \
-            ruby-dev \
-            python3-pip \
-            libnl-3-dev cmake snapd libboost-all-dev libnl-genl-3-dev \
-            libnl-route-3-dev || exit 1
-
+apt -y install curl git ruby-dev python3-pip || exit 1
 
 gem install fpm
 pip install --upgrade cloudsmith-cli
 
-apt -y install fmt
